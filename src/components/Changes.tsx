56b88f936e8cbf6637cabf78c3b220571694a962
Marcus Oates <marcus.oates@downergroup.com>
2025-10-25 00:21:33 +1100

    save progress

--- src/components/Changes.tsx
+++ src/components/Changes.tsx
@@ -1,6 +1,12 @@
 import { JSX } from "react"
-import { ChipKey } from "../middleware/chipMap"
+import { ChipKey, getChip } from "../middleware/chipMap"
 import { iconMap } from "../pages/Home"
+import { Accordion, AccordionDetails, AccordionSummary, Avatar, Box, IconButton, Stack, Typography } from "@mui/material"
+import { buildBulletPoints } from "../middleware/helpers"
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export type ChangeProject = 'finska' | 'gym_junkie';
 
 export interface ChangeData {
   // icon: string
@@ -8,13 +14,11 @@ export interface ChangeData {
   points: string[]
 }
 
-export interface RoadmapData extends ChangeData {
+export interface RoadmapBareData extends ChangeData {
   chipKey: ChipKey
 }
 
-export type ChangeProject = 'finska' | 'gym_junkie';
-
-export const roadmapData: Record<ChangeProject, RoadmapData[]> = {
+export const roadmapData: Record<ChangeProject, RoadmapBareData[]> = {
   finska: [
     {
       // icon: iconMap.finska,
@@ -50,6 +54,10 @@ export const roadmapData: Record<ChangeProject, RoadmapData[]> = {
   gym_junkie: []
 }
 
+export interface RoadmapData extends RoadmapBareData {
+  icon: string
+}
+
 export interface ReleaseData extends ChangeData {
   version: string
   link: string
@@ -82,7 +90,195 @@ export const releaseData: Record<ChangeProject, ReleaseData[]> = {
   gym_junkie: [],
 }
 
-// todo build roadmap, build release
+export const buildRoadmap = (roadmap: RoadmapData[], maxHeight?: number): JSX.Element => {
+  return (
+    <>
+      <Typography variant="h6" sx={{paddingBottom: '5px'}}>
+        Roadmap
+      </Typography>
+      <Box
+        sx={{...(maxHeight && {
+          maxHeight: maxHeight,
+          overflowY: 'auto',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+        })}}
+      >
+        <Stack
+          spacing={1.5}
+        >
+          {roadmap.map((data, i) => {
+            return (
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                >
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      width: '100%',
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Avatar 
+                        alt="icon" 
+                        src={data.icon}
+                        sx={{ 
+                          width: 24, 
+                          height: 24, 
+                          marginRight: 1.5,
+                        }}
+                      />
+                      <Typography>
+                        {data.header}
+                      </Typography>
+                    </Box>
+                    <Box sx={{marginRight: 0.5}}>
+                      {getChip(data.chipKey)}
+                    </Box>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {buildBulletPoints(data.points)}
+                </AccordionDetails>
+              </Accordion>
+            )
+          })}
+        </Stack>
+      </Box>
+    </>
+  )
+};
+
+export const buildProjectRoadmap = (project: ChangeProject, maxHeight?: number): JSX.Element => {
+  const roadmap: RoadmapData[] = roadmapData[project].map((item) => ({
+    ...item,
+    icon: iconMap[project]
+  }));
+  return buildRoadmap(roadmap, maxHeight);
+};
+
+export const buildAllRoadmap = (maxHeight?: number): JSX.Element => {
+  const roadmap: RoadmapData[] = [];
+  let i = 0;
+  while (1) {
+    let isExhausted = true;
+    for (const [project, data] of Object.entries(roadmapData)) {
+      if (i >= data.length) continue;
+      isExhausted = false;
+      roadmap.push({
+        ...data[i],
+        icon: iconMap[project as  ChangeProject]
+      })
+    }
+    if (isExhausted) break;
+    i++;
+  }
+  return buildRoadmap(roadmap, maxHeight);
+};
+
+
+
+export const buildReleases = (project: ChangeProject, maxHeight?: number): JSX.Element => {
+  return (
+    <>
+      <Typography variant="h6" sx={{paddingBottom: '5px'}}>
+        Releases
+      </Typography>
+      <Box
+        sx={{...(maxHeight && {
+          maxHeight: maxHeight,
+          overflowY: 'auto',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+        })}}
+      >
+        <Stack
+          spacing={1.5}
+        >
+          {releaseData[project].map((data, i) => {
+            return (
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                >
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      width: '100%',
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Avatar 
+                        alt="icon" 
+                        src={iconMap[project]}
+                        sx={{ 
+                          width: 24, 
+                          height: 24, 
+                          marginRight: 1.5,
+                        }}
+                      />
+                      <Typography>
+                        {data.header}
+                      </Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                        marginRight: 1,
+                      }}
+                    >
+                      <Typography sx={{marginRight: 1}}>
+                        {data.version}
+                      </Typography>
+                      <IconButton 
+                        href={data.link} 
+                        target="_blank" 
+                        rel="noopener"
+                        sx={{
+                          width: 20,
+                          height: 20,
+                        }}
+                      >
+                        <OpenInNewIcon sx={{ fontSize: 20 }}/>
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {buildBulletPoints(data.points)}
+                </AccordionDetails>
+              </Accordion>
+            )
+          })}
+        </Stack>
+      </Box>
+    </>
+  );
+};
 
 // const listAll = () => {
 //   while (1) {
